64801c2948ac79ae5f490c18b13efe68a522691d
Kristin Kleyla <clever_disguises@yahoo.com>
2022-07-26 18:20:54 -0400

    added basic styling to About.js and fixed positioning of Footer

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function About() {
   return (
-    <h1>About</h1>
+    <div className='row mt-5'>
+      <h2 className='col-12 text-center'>About Me</h2>
+      <img src='/images/profile-pic.jpg' alt='Headshot' className='col-2 rounded-circle m-3'></img>
+      <p className='col-12 col-md-9 mt-5'>Software developer with experience leading teams up to twenty people to maximize efficiency and oversee collaborative creative environments. Previous experience includes planning and launching a private business that involved resource and time management, customer service skills, and active problem-solving. Maintains a high standard of work ethic and self-teaching. Currently enrolled in the Full Stack Web Development course at UNC Charlotte Boot Camp, set to receive a certificate in August, 2022 for front and back-end development including JavaScript, Express, and RESTful APIs. Excited to apply skills in problem-solving and leadership to software development while continuing to learn and flourish.</p>
+    </div>
   )
 };
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className='row d-flex justify-content-center'>
-      <div className='col-5 p-3 text-center'>
+    <footer className='row d-flex text-center'>
+      <div className='col-5 p-3 m-auto fixed-bottom'>
         <a href='https://github.com/kleylakb89'><img src='/images/github-logo.png' alt='GitHub' className='col-1 p-1'></img></a>
         <a href='https://www.linkedin.com/in/kristin-kleyla/'><img src='/images/linkedin-logo.png' alt='LinkedIn' className='col-1 p-1'></img></a>
         <a href='https://www.twitch.tv/normalnotincluded'><img src='/images/twitch-logo.png' alt='Twitch' className='col-1 p-1'></img></a>
