0d166786a1e9f23f528b3647522f66034902e23a
Kristin Kleyla <clever_disguises@yahoo.com>
2022-07-26 19:19:18 -0400

    added projects to porfolio and basic styling

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import Project from './Project';
+
+const projects = [
+  {
+    title: 'BookNook',
+    image: '/images/book-nook.png',
+    github: 'https://github.com/kleylakb89/book-nook',
+    deployed: 'https://dry-bastion-57106.herokuapp.com/'
+  },
+  {
+    title: 'Pantry Prepper',
+    image: '/images/pantry-prepper-landing-page.png',
+    github: 'https://github.com/kleylakb89/pantry-prepper',
+    deployed: 'https://kleylakb89.github.io/pantry-prepper/'
+  },
+  {
+    title: 'PWA Text Editor',
+    image: '/images/text-editor.png',
+    github: 'https://github.com/kleylakb89/19-pwa-text-editor',
+    deployed: 'https://blooming-shelf-15321.herokuapp.com/'
+  },
+  {
+    title: 'Tech Blog',
+    image: '/images/blog-homepage.png',
+    github: 'https://github.com/kleylakb89/14-tech-blog',
+    deployed: 'https://morning-refuge-95641.herokuapp.com/'
+  },
+  {
+    title: 'Employee Tracker',
+    image: '/images/employee-tracker.png',
+    github: 'https://github.com/kleylakb89/12-employee-tracker',
+    deployed: 'https://drive.google.com/file/d/1EnTavy77JiLVqjEp2L8-BcIJaMwuExiA/view'
+  },
+  {
+    title: 'Note Taker',
+    image: '/images/note-landing-page.png',
+    github: 'https://github.com/kleylakb89/11-note-taker',
+    deployed: 'https://damp-sierra-92977.herokuapp.com/'
+  },
+]
 
 function Portfolio() {
   return (
-    <h1>Portfolio</h1>
+    <>
+      <h2 className='text-center'>Portfolio</h2>
+      <Project projects={projects} />
+    </>
   )
 };
 
---
+++ src/components/Project.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const styles = {
+  card: {
+    width: '50rem',
+    margin: '50px auto'
+  },
+}
+
+function Project(props) {
+  return (
+    <div>
+      {props.projects.map((single) => {
+        return (
+          <div className='card' style={styles.card}>
+            <img src={single.image} alt='Landing Page' className='card-img-top'></img>
+            <div className='card-body text-center'>
+              <a href={single.deployed} className='card-title'><h4>{single.title}</h4></a>
+              <a href={single.github} className='card-text'><img src='images/github-logo.png' alt='github link' className='col-1'></img></a>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+};
+
+export default Project;
