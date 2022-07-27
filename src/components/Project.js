a0f2faadceb13ef03678191c3459f4d1d36cf6bf
Kristin Kleyla <clever_disguises@yahoo.com>
2022-07-26 20:26:36 -0400

    added downloadable resume and techincal skills to resume page

--- src/components/Project.js
+++ src/components/Project.js
@@ -12,7 +12,7 @@ function Project(props) {
     <div>
       {props.projects.map((single) => {
         return (
-          <div className='card' style={styles.card}>
+          <div className='card' style={styles.card} key={single.title}>
             <img src={single.image} alt='Landing Page' className='card-img-top'></img>
             <div className='card-body text-center'>
               <a href={single.deployed} className='card-title'><h4>{single.title}</h4></a>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 function Resume() {
   return (
-    <h1>Resume</h1>
+    <div className='row text-center'>
+      <h2>Resume</h2>
+      <a href='resume.docx' download>Download My Resume!</a>
+      <h4>Technical Skills</h4>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>RESTful APIs</li>
+        <li>Bootstrap</li>
+        <li>Express.js</li>
+        <li>Handlebars</li>
+        <li>MySQL</li>
+        <li>Sequelize</li>
+        <li>NoSQL</li>
+        <li>MongoDB</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
   )
 };
 
